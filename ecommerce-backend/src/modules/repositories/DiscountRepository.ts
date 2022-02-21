90781a2b6607d4e5f64ae5e836df2dbebfc6a68c
Lino Veloso <linoc.veloso@gmail.com>
2022-02-20 23:46:58 -0300

    creating first usecase

---
+++ ecommerce-backend/src/modules/repositories/DiscountRepository.ts
@@ -0,0 +1,1 @@
+class DiscountRepository implements IDiscountRepository {}
--- ecommerce-backend/src/modules/useCases/applyDiscount/index.ts
+++ ecommerce-backend/src/modules/useCases/applyDiscount/index.ts
@@ -1,1 +1,6 @@
-const applyDiscountController = new ApplyDiscountController()
+import { ApplyDiscountController } from "./ApplyDiscountController";
+const discountRepository = DiscountRepository.getInstance();
+const applyDiscountUseCase = new ApplyDiscountUseCase(discountRepository)
+const applyDiscountController = new ApplyDiscountController(applyDiscountUseCase)
+
+export {applyDiscountController}
