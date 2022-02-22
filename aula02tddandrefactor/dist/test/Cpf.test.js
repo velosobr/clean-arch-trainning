0670e6b4019169701d243bb16b0fa986c8f24aa2
Lino Veloso <linoc.veloso@gmail.com>
2022-02-21 22:19:17 -0300

    Creating order and unit test

--- aula02tddandrefactor/dist/test/Cpf.test.js
+++ aula02tddandrefactor/dist/test/Cpf.test.js
@@ -8,3 +8,12 @@ test("Deve testar um CPF válido", function () {
     const cpf = new Cpf_1.default("089.608.269-52");
     expect(cpf.getValue()).toBe("089.608.269-52");
 });
+const invalidCpfWithSameDigit = [
+    "111.111.111-11",
+    "222.222.222-22"
+];
+describe.each(invalidCpfWithSameDigit)("Deve testar um cpf inválido com digitos iguais", function (cpf) {
+    test(`${cpf}`, function () {
+        expect(() => new Cpf_1.default(cpf)).toThrow(new Error("CPF inválido"));
+    });
+});
--- aula02tddandrefactor/test/Cpf.test.ts
+++ aula02tddandrefactor/test/Cpf.test.ts
@@ -4,3 +4,13 @@ test("Deve testar um CPF válido", function(){
 const cpf = new Cpf("089.608.269-52")
 expect(cpf.getValue()).toBe("089.608.269-52")
 })
+
+const invalidCpfWithSameDigit = [
+   "111.111.111-11",
+   "222.222.222-22"
+]
+describe.each(invalidCpfWithSameDigit)("Deve testar um cpf inválido com digitos iguais",function(cpf){
+   test(`${cpf}`, function () {
+      expect(() => new Cpf(cpf)).toThrow(new Error("CPF inválido"))
+   })   
+})
