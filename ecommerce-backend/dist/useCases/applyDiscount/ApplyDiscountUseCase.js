9a2477ef5361bcc94fd252ab096753bd54d26d1a
lino.veloso <lino.veloso@sensedia.com>
2024-01-10 14:35:45 -0300

    start of trainning 14 - clean folders

--- ecommerce-backend/dist/useCases/applyDiscount/ApplyDiscountUseCase.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ApplyDiscountUseCase = void 0;
-class ApplyDiscountUseCase {
-}
-exports.ApplyDiscountUseCase = ApplyDiscountUseCase;
--- ecommerce-backend/dist/useCases/applyDiscount/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.applyDiscountController = void 0;
-const ApplyDiscountController_1 = require("./ApplyDiscountController");
-const discountRepository = DiscountRepository.getInstance();
-const applyDiscountUseCase = new ApplyDiscountUseCase(discountRepository);
-const applyDiscountController = new ApplyDiscountController_1.ApplyDiscountController(applyDiscountUseCase);
-exports.applyDiscountController = applyDiscountController;
